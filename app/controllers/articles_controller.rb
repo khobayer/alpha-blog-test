69ec117787e7a296cc9d7d04e77c591523fb8e11
khobayer <khobayerkhan@gmail.com>
2023-12-11 16:51:30 +0600

    complete add new article with alrtes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,11 +9,17 @@ class ArticlesController < ApplicationController
     end
 
     def new
-    
+        @article = Articale.new
     end
 
     def create
-    
+        @article = Articale.new(params.require(:articale).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was successfully added"
+            redirect_to article_path(@article)
+        else
+            render 'new', status: :unprocessable_entity
+        end
     end
 
 end
